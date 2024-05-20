e791a10bcefd44d2257f5331bb3fe8a6bd74353c
Darnell <darnjames@gmail.com>
2024-05-20 01:23:23 -0400

    Remove bug where all timestamps would be the same value

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const PostSchema = new Schema({
   title: { type: String, required: true, maxLength: 100 },
   content: { type: String, required: true, maxLength: 1000 },
-  timestamp: { type: Number, default: Date.now() },
+  timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
